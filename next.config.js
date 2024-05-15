8aa781a9570ecc2ff9d773636316501d09f4ec8d
JTHuang-Nunu <ge6435wr@gmail.com>
2024-05-15 19:44:32 +0800

    rebuild

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
 })
 
-// const nextConfig = {
-//   // basePath: process.env.BASE_PATH,
-//   // assetPrefix: process.env.ASSET_PREFIX,
-//   // reactStrictMode: true,
-//   output: 'export',
-//   distDir: 'dist',
-//   // images: {
-//   //   unoptimized: true,
-//   // }
-// };
-
-module.exports = withNextra(nextConfig);
+const nextConfig = {
+  basePath: process.env.BASE_PATH,
+  assetPrefix: process.env.ASSET_PREFIX,
+  reactStrictMode: true,
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+}
+  
+module.exports = withNextra(nextConfig)
