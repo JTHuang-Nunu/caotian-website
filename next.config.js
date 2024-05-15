fff10eae97b5a2033f463526743762d19ed24709
JTHuang-Nunu <ge6435wr@gmail.com>
2024-05-15 14:15:48 +0800

    usign distDir in next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   assetPrefix: process.env.ASSET_PREFIX,
   reactStrictMode: true,
   output: 'export',
-  // distDir: 'dist',
+  distDir: 'dist',
   images: {
     unoptimized: true,
   }
