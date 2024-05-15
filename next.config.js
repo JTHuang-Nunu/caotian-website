1dc252a8a64ed77104abb16b0510e7bce3f4269d
JTHuang-Nunu <ge6435wr@gmail.com>
2024-05-15 21:48:22 +0800

    modify Image component path-2

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const isProd = process.env.NODE_ENV === 'production'
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
@@ -7,7 +8,7 @@ const nextConfig = {
   basePath: process.env.BASE_PATH,
   assetPrefix: process.env.ASSET_PREFIX,
   reactStrictMode: true,
-  // output: 'export',
+  output: isProd ? 'export' : 'standalone',
   distDir: 'dist',
   images: {
     unoptimized: true,
